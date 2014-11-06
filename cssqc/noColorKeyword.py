4058df33ace180527f0d151ea84e159adb2c82c6
matematik7 <domen.ipavec@z-v.si>
2014-11-06 13:01:17 +0100

    Added noColrKeyword rule.
    Signed-off-by: matematik7 <domen.ipavec@z-v.si>

---
+++ cssqc/noColorKeyword.py
@@ -0,0 +1,172 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# ----------------------------------------------------------------
+# cssqc/noColorKeyword.py
+# 
+# Do not allow standard css color keywords.
+# ----------------------------------------------------------------
+# copyright (c) 2014 - Domen Ipavec
+# Distributed under The MIT License, see LICENSE
+# ----------------------------------------------------------------
+
+from cssqc import QualityWarning
+from bisect import bisect_left
+
+class noColorKeyword:
+    def __init__(self, data):
+        self.colors = (
+            'aliceblue',
+            'antiquewhite',
+            'aqua',
+            'aquamarine',
+            'azure',
+            'beige',
+            'bisque',
+            'black',
+            'blanchedalmond',
+            'blue',
+            'blueviolet',
+            'brown',
+            'burlywood',
+            'cadetblue',
+            'chartreuse',
+            'chocolate',
+            'coral',
+            'cornflowerblue',
+            'cornsilk',
+            'crimson',
+            'cyan',
+            'darkblue',
+            'darkcyan',
+            'darkgoldenrod',
+            'darkgray',
+            'darkgreen',
+            'darkkhaki',
+            'darkmagenta',
+            'darkolivegreen',
+            'darkorange',
+            'darkorchid',
+            'darkred',
+            'darksalmon',
+            'darkseagreen',
+            'darkslateblue',
+            'darkslategray',
+            'darktorquoise',
+            'darkviolet',
+            'deeppink',
+            'deepskyblue',
+            'dimgray',
+            'dodgerblue',
+            'firebrick',
+            'floralwhite',
+            'forestgreen',
+            'fuchsia',
+            'gainsboro',
+            'ghostwhite',
+            'gold',
+            'goldenrod',
+            'gray',
+            'green',
+            'greenyellow',
+            'honeydew',
+            'hotpink,'
+            'indianred',
+            'indigo',
+            'ivory',
+            'khaki',
+            'lavender',
+            'lavenderblush',
+            'lawngreen',
+            'lemonchiffon',
+            'lightblue',
+            'lightcoral',
+            'lightcyan',
+            'lightgoldenrodyellow',
+            'lightgray',
+            'lightgreen',
+            'lightpink',
+            'lightsalmon',
+            'lightseagreen',
+            'lightskyblue',
+            'lightslategray',
+            'lightsteelblue',
+            'lightyellow',
+            'lime',
+            'limegreen',
+            'linen',
+            'magenta',
+            'maroon',
+            'mediumaquamarine',
+            'mediumblue',
+            'mediumorchid',
+            'mediumpurple',
+            'mediumseagreen',
+            'mediumslateblue',
+            'mediumspringgreen',
+            'mediumtorquoise',
+            'mediumvioletred',
+            'mignightblue',
+            'mintcream',
+            'mistyrose',
+            'moccasin',
+            'navajowhite',
+            'navy',
+            'oldlace',
+            'olive',
+            'olivedrab',
+            'orange',
+            'orangered',
+            'orchid',
+            'palegoldenrod',
+            'palegreen',
+            'paleturquoise',
+            'palevioletred',
+            'papayawhip',
+            'peachpuff',
+            'peru',
+            'pink',
+            'plum',
+            'powderblue',
+            'purple',
+            'red',
+            'rosybrown',
+            'royalblue',
+            'saddlebrown',
+            'salmon',
+            'sandybrown',
+            'seagreen',
+            'seashell',
+            'sienna',
+            'silver',
+            'skyblue',
+            'slateblue',
+            'slategray',
+            'snow',
+            'springgreen',
+            'steelblue',
+            'tan',
+            'teal',
+            'thistle', 
+            'tomato',
+            'turquoise',
+            'violet',
+            'wheat',
+            'white',
+            'whitesmoke',
+            'yellow',
+            'yellowgreen'
+        )
+        
+    def isColor(self, v):
+        v = v.lower()
+        i = bisect_left(self.colors, v)
+        return i != len(self.colors) \
+            and self.colors[i] == v
+        
+    def on_Statement(self, statement):
+        warnings = []
+        for t in statement.text:
+            if type(t) is tuple \
+                and self.isColor(t[0]):
+                warnings.append(QualityWarning('noColorKeyword', t[1], 'Color keyword "%s" appears.' % t[0]))
+        return warnings
---
+++ tests/test_noColorKeyword.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# ----------------------------------------------------------------
+# test_noColorKeyword.py
+# 
+# test for noColorKeyword rule
+# ----------------------------------------------------------------
+# copyright (c) 2014 - Domen Ipavec
+# Distributed under The MIT License, see LICENSE
+# ----------------------------------------------------------------
+
+import unittest
+from cssqc import CSSQC, QualityWarning
+
+class Test_noColorKeyword(unittest.TestCase):
+    def parse(self, data):
+        c = CSSQC({"noColorKeyword": True})
+        c.parse(data)
+        return c
+    
+    def test_no_color_keyword(self):
+        c = self.parse('''// Variables
+@link-color:        Blue;
+@link-color-hover:  darken(@link-color, 10%);
+
+// Usage
+a,
+.link {
+  color: @link-color;
+}
+a:hover {
+  color: @link-color-hover;
+}
+.widget {
+  color: black;
+  background: @link-color;
+}
+.black {
+    color: #000;
+}''')
+        self.assertEqual(c.warnings, [
+            QualityWarning('noColorKeyword', 2),
+            QualityWarning('noColorKeyword', 14)
+        ])
