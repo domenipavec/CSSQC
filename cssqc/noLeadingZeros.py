7b60d2c473e60d982840811060a05fdca2578adc
matematik7 <domen.ipavec@z-v.si>
2014-11-10 09:58:47 +0100

    Added noLeadingZeros rule.
    Signed-off-by: matematik7 <domen.ipavec@z-v.si>

---
+++ cssqc/noLeadingZeros.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# ----------------------------------------------------------------
+# cssqc/noLeadingZeros.py
+# 
+# Do not allow leading zeros with decimals (e.g. 0.3px).
+# ----------------------------------------------------------------
+# copyright (c) 2014 - Domen Ipavec
+# Distributed under The MIT License, see LICENSE
+# ----------------------------------------------------------------
+
+from cssqc import QualityWarning
+from csslex import ident, flags
+
+import re
+
+class noLeadingZeros:
+    def __init__(self, data):
+        self.on_PERCENTAGE = self.on_DIMENSION
+        self.on_NUMBER = self.on_DIMENSION
+        self.leading_zeros_re = re.compile(flags + r'(0+\.[0-9]*)(%|('+ident+r'))?')
+
+    def on_DIMENSION(self, i):
+        print(i.value)
+        if self.leading_zeros_re.match(i.value):
+            return [QualityWarning('noLeadingZeros', i.lineno, 'A leading zero appears: ' + i.value)]
+        else:
+            return []
---
+++ tests/test_noLeadingZeros.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# ----------------------------------------------------------------
+# test_noLeadingZeros.py
+# 
+# test for noLeadingZeros rule
+# ----------------------------------------------------------------
+# copyright (c) 2014 - Domen Ipavec
+# Distributed under The MIT License, see LICENSE
+# ----------------------------------------------------------------
+
+import unittest
+from cssqc import CSSQC, QualityWarning
+
+class Test_noLeadingZeros(unittest.TestCase):
+    def parse(self, data):
+        c = CSSQC({"noLeadingZeros": True})
+        c.parse(data)
+        return c
+    
+    def test_no_zero_units(self):
+        c = self.parse('''div {
+    height: 0.1px;
+    width: 0.2%;
+    margin-top: 0px;
+    margin-bottom: 10.0em;
+    margin-left: .0em;
+    margin-right: .0%;
+    padding-top: 00.3px;
+    padding-right: 00.3;
+    padding-left: .00px;
+    padding-bottom: .0;
+}''')
+        self.assertEqual(c.warnings, [
+            QualityWarning('noLeadingZeros', 2),
+            QualityWarning('noLeadingZeros', 3),
+            QualityWarning('noLeadingZeros', 8),
+            QualityWarning('noLeadingZeros', 9)
+        ])
