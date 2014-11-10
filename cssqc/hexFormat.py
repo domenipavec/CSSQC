7b4a931e96eba4b78b01b13c3ba06d6d6b3e4791
matematik7 <domen.ipavec@z-v.si>
2014-11-10 11:50:35 +0100

    Added hexFormat rule.
    Signed-off-by: matematik7 <domen.ipavec@z-v.si>

---
+++ cssqc/hexFormat.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# ----------------------------------------------------------------
+# cssqc/hexFormat.py
+# 
+# Format hex colors.
+# Options are 'long' (6character hex code only), 
+# 'short' (use 3 character hex when possible),
+# 'uppercase', 'lowercase', 
+# and 'validate' (warning if not 3 or 6 character)
+# ----------------------------------------------------------------
+# copyright (c) 2014 - Domen Ipavec
+# Distributed under The MIT License, see LICENSE
+# ----------------------------------------------------------------
+
+from cssqc import QualityWarning
+from cssqc.helpers import isTupleWithValue
+from bisect import bisect_left
+
+class hexFormat:
+    def __init__(self, data):
+        self.on_Parentheses = self.on_Function
+        if 'long' in data:
+            self.check_long = True
+        else:
+            self.check_long = False
+        if 'short' in data:
+            self.check_short = True
+        else:
+            self.check_short = False
+        if 'uppercase' in data:
+            self.check_upper = True
+        else:
+            self.check_upper = False
+        if 'lowercase' in data:
+            self.check_lower = True
+        else:
+            self.check_lower = False
+        if 'validate' in data:
+            self.check_validate = True
+        else:
+            self.check_validate = False
+
+    def isInvalidColor(self, v):
+        vlower = v.lower()
+        if v[0] == '#':
+            if self.check_long and len(v) == 4:
+                return True
+            if self.check_short \
+                and len(v) == 7 \
+                and vlower[1] == vlower[2] \
+                and vlower[3] == vlower[4] \
+                and vlower[5] == vlower[6]:
+                return True
+            if self.check_lower \
+                and (len(v) == 7 or len(v) == 4) \
+                and vlower != v:
+                return True
+            if self.check_upper \
+                and (len(v) == 7 or len(v) == 4) \
+                and v.upper() != v:
+                return True
+            if self.check_validate \
+                and not (len(v) == 7 or len(v) == 4):
+                return True
+        else:
+            return False
+
+    def on_Function(self, f):
+        warnings = []
+        for t in f.text:
+            if type(t) is tuple \
+                and self.isInvalidColor(t[0]):
+                warnings.append(QualityWarning('hexFormat', t[1], 'Wrong formatted hex "%s" appears.' % t[0]))
+        return warnings
+    
+    def on_Statement(self, statement):
+        warnings = []
+        is_after_colon = False
+        for t in statement.text:
+            if is_after_colon:
+                if type(t) is tuple \
+                    and self.isInvalidColor(t[0]):
+                    warnings.append(QualityWarning('hexFormat', t[1], 'Wrong formatted hex "%s" appears.' % t[0]))
+            else:
+                if isTupleWithValue(t, ':'):
+                    is_after_colon = True
+        return warnings
---
+++ tests/test_hexFormat.py
@@ -0,0 +1,158 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# ----------------------------------------------------------------
+# test_hexFormat.py
+# 
+# test for hexFormat rule
+# ----------------------------------------------------------------
+# copyright (c) 2014 - Domen Ipavec
+# Distributed under The MIT License, see LICENSE
+# ----------------------------------------------------------------
+
+import unittest
+from cssqc import CSSQC, QualityWarning
+
+class Test_hexFormat(unittest.TestCase):
+    def parse(self, data, l):
+        c = CSSQC({"hexFormat": l})
+        c.parse(data)
+        return c
+
+    def assertItemsEqual(self, l1, l2):
+        self.assertEqual(sorted(l1, key=QualityWarning.getLine), sorted(l2, key=QualityWarning.getLine))
+
+    def parseSample(self, l):
+        sample = '''/* variables */
+@c1: #f2f;
+@c2: #F2F;
+@c3: #f2F;
+@c4: #FE23AB;
+@c5: #ff23ff;
+@c6: #FE22ff;
+@c7: #ff22ff;
+@c8: #f3;
+@c9: #ff89b;
+/* ids, colors and mixins */
+#f2f {
+    color: #f2f;
+    #f2f;
+}
+#F2F {
+    color: #F2F;
+    #F2F;
+}
+#f2F {
+    color: #f2F;
+    #f2F;
+}
+#ff22ff {
+    color: #ff22ff;
+    #ff22ff;
+}
+#f2fe {
+    color: #f2fe;
+    #f2fe;
+}
+/* parameters */
+.foo (@bg: #f2f) {
+  background: @bg;
+}
+.foo (@bg: #F2F) {
+  background: @bg;
+}
+.foo (@bg: #f2F) {
+  background: @bg;
+}
+.foo (@bg: #ff22ff) {
+  background: @bg;
+}
+.foo (@bg: #ff2ef) {
+  background: @bg;
+}'''
+        warnings_dict = {
+            'long': [
+                QualityWarning('hexFormat', 2),
+                QualityWarning('hexFormat', 3),
+                QualityWarning('hexFormat', 4),
+                QualityWarning('hexFormat', 13),
+                QualityWarning('hexFormat', 17),
+                QualityWarning('hexFormat', 21),
+                QualityWarning('hexFormat', 33),
+                QualityWarning('hexFormat', 36),
+                QualityWarning('hexFormat', 39)
+            ],
+            'short': [
+                QualityWarning('hexFormat', 8),
+                QualityWarning('hexFormat', 25),
+                QualityWarning('hexFormat', 42)
+            ],
+            'lowercase': [
+                QualityWarning('hexFormat', 3),
+                QualityWarning('hexFormat', 4),
+                QualityWarning('hexFormat', 5),
+                QualityWarning('hexFormat', 7),
+                QualityWarning('hexFormat', 17),
+                QualityWarning('hexFormat', 21),
+                QualityWarning('hexFormat', 36),
+                QualityWarning('hexFormat', 39)
+            ],
+            'uppercase': [
+                QualityWarning('hexFormat', 2),
+                QualityWarning('hexFormat', 4),
+                QualityWarning('hexFormat', 6),
+                QualityWarning('hexFormat', 7),
+                QualityWarning('hexFormat', 8),
+                QualityWarning('hexFormat', 13),
+                QualityWarning('hexFormat', 21),
+                QualityWarning('hexFormat', 25),
+                QualityWarning('hexFormat', 33),
+                QualityWarning('hexFormat', 39),
+                QualityWarning('hexFormat', 42)
+            ],
+            'validate': [
+                QualityWarning('hexFormat', 9),
+                QualityWarning('hexFormat', 10),
+                QualityWarning('hexFormat', 29),
+                QualityWarning('hexFormat', 45)
+            ]
+        }
+        c = self.parse(sample, l)
+        warnings = []
+        for i in l:
+            for w in warnings_dict[i]:
+                if w not in warnings:
+                    warnings.append(w)
+        self.assertItemsEqual(c.warnings, warnings)
+    
+    def test_nothing(self):
+        self.parseSample([])
+    
+    def test_long(self):
+        self.parseSample(['long'])
+    
+    def test_short(self):
+        self.parseSample(['short'])
+    
+    def test_lowercase(self):
+        self.parseSample(['lowercase'])
+    
+    def test_uppercase(self):
+        self.parseSample(['uppercase'])
+    
+    def test_validate(self):
+        self.parseSample(['validate'])
+
+    def test_combinations(self):
+        cs = (('long', 'short'), ('lowercase', 'uppercase'))
+        for i in range(3):
+            for j in range(3):
+                for k in range(2):
+                    l = []
+                    if i < 2:
+                        l.append(cs[0][i])
+                    if j < 2:
+                        l.append(cs[1][j])
+                    if k < 1:
+                        l.append('validate')
+                    if len(l) > 1:
+                        self.parseSample(l)
